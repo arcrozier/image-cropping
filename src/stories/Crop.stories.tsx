95ea1ae85cdffb90972bf630e514c761b148e724
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2023-07-29 21:36:50 -0700

    Added test cases and removed dependency on random external URL

---
+++ src/stories/Crop.stories.tsx
@@ -0,0 +1,105 @@
+import React from "react";
+import { StoryFn, Meta } from "@storybook/react";
+import Crop from "../components/Crop/Crop";
+// @ts-ignore
+import landscape from "./assets/landscape.jpg"
+// @ts-ignore
+import portrait from "./assets/portrait.jpg"
+// @ts-ignore
+import square from "./assets/square.jpg"
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: "ReactComponentLibrary/Crop",
+    component: Crop,
+} as Meta<typeof Crop>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: StoryFn<typeof Crop> = (args) => <div style={{height: "500px", width: "900"}}><Crop {...args} /></div>;
+
+export const LandscapeFreeAspect = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+const placeholderRef = {current: undefined}
+LandscapeFreeAspect.args = {
+    src: landscape,
+    renderer: placeholderRef,
+    thirds: true
+};
+
+export const LandscapePortraitAspect = Template.bind({});
+LandscapePortraitAspect.args = {
+    src: landscape,
+    renderer: placeholderRef,
+    aspect: 1 / 2
+};
+
+export const LandscapeLandscapeAspect = Template.bind({});
+LandscapeLandscapeAspect.args = {
+    src: landscape,
+    renderer: placeholderRef,
+    aspect: 2
+};
+
+export const LandscapeSquareAspect = Template.bind({});
+LandscapeSquareAspect.args = {
+    src: landscape,
+    renderer: placeholderRef,
+    aspect: 1
+};
+
+export const PortraitFreeAspect = Template.bind({})
+PortraitFreeAspect.args = {
+    src: portrait,
+    renderer: placeholderRef,
+    thirds: true
+};
+
+export const PortraitPortraitAspect = Template.bind({});
+PortraitPortraitAspect.args = {
+    src: portrait,
+    renderer: placeholderRef,
+    aspect: 1 / 2
+};
+
+export const PortraitLandscapeAspect = Template.bind({});
+PortraitLandscapeAspect.args = {
+    src: portrait,
+    renderer: placeholderRef,
+    aspect: 2
+};
+
+export const PortraitSquareAspect = Template.bind({});
+PortraitSquareAspect.args = {
+    src: portrait,
+    renderer: placeholderRef,
+    aspect: 1
+};
+
+export const SquareFreeAspect = Template.bind({})
+SquareFreeAspect.args = {
+    src: square,
+    renderer: placeholderRef,
+    thirds: true
+};
+
+export const SquarePortraitAspect = Template.bind({});
+SquarePortraitAspect.args = {
+    src: square,
+    renderer: placeholderRef,
+    aspect: 1 / 2
+};
+
+export const SquareLandscapeAspect = Template.bind({});
+SquareLandscapeAspect.args = {
+    src: square,
+    renderer: placeholderRef,
+    aspect: 2
+};
+
+export const SquareSquareAspect = Template.bind({});
+SquareSquareAspect.args = {
+    src: square,
+    renderer: placeholderRef,
+    aspect: 1
+};
